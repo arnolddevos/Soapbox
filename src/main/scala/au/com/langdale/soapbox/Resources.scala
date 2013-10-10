af590d776040afa1fd336a10c01c518dda83dd13
Arnold deVos <adv@langdale.com.au>
2013-10-10 12:09:08 +1100

    add support for the lib directory

--- src/main/scala/au/com/langdale/soapbox/Resources.scala
+++ src/main/scala/au/com/langdale/soapbox/Resources.scala
@@ -11,31 +11,39 @@ object Resources extends Plugin {
   
   val resourceBuild       = taskKey[Seq[File]]("copy resource files into site")
   val resourceSources     = taskKey[Seq[File]]("directories for resource source files")
+  val resourceLibraries   = taskKey[File]("directory for third party resource libraries. each subdirectory will be added to resourceSources")
   val resourceFilter      = taskKey[FileFilter]("wildcard expression matching resource file names")
   val resourceImageFilter = taskKey[FileFilter]("wildcard expression matching image file names")
   val resourcePageFilter  = taskKey[FileFilter]("wildcard expression matching HTML file names")
   val resourceExclusions  = taskKey[PathFinder]("specifies paths to ignore when copying resources")
+  val resourceMap         = taskKey[Seq[(File, String)]]("resource files paired theit site relative path names")
 
   def mapPaths( roots: Seq[File], exclude: PathFinder, include: FileFilter): Seq[(File, String)] = {
     for {
       root <- roots
-      pair <- root ** include --- exclude pair relativeTo(root)
+      pair <- root ** include --- exclude pair (relativeTo(root), false)
     }
     yield pair
   }
 
   override def projectSettings = Seq(
 
+    resourceLibraries   := baseDirectory.value / "lib",
     resourceSources     := siteSources.value,
     resourceFilter      := "*.css" | "*.pdf" | "*.ico" | "*.js" | "*.woff" | "*.ttf",
     resourceImageFilter := "*.png" | "*.gif" | "*.jpg" | "*.svg",
     resourcePageFilter  := "*.html",
     resourceExclusions  := PathFinder.empty,
 
+    resourceMap := 
+      mapPaths(resourceSources.value, resourceExclusions.value, 
+          resourceFilter.value || resourceImageFilter.value || resourcePageFilter.value) ++
+      mapPaths((resourceLibraries.value * DirectoryFilter).get, resourceExclusions.value, 
+          resourceFilter.value || resourceImageFilter.value ),
+
     resourceBuild := {
       for {
-        (src, path) <- mapPaths(resourceSources.value, resourceExclusions.value, 
-          resourceFilter.value || resourceImageFilter.value || resourcePageFilter.value)
+        (src, path) <- resourceMap.value 
         dst = siteProduct.value / path
       }
       yield {
@@ -45,17 +53,17 @@ object Resources extends Plugin {
     },
 
     siteResources ++= {
-      for((_, dst) <- mapPaths(resourceSources.value, resourceExclusions.value, resourceFilter.value))
+      for((src, dst) <-resourceMap.value if resourceFilter.value accept src)
         yield dst
     },
 
     siteImages ++= {
-      for((_, dst) <- mapPaths(resourceSources.value, resourceExclusions.value, resourceImageFilter.value))
+      for((src, dst) <-resourceMap.value if resourceImageFilter.value accept src)
         yield dst
     },
 
     sitePages ++= {
-      for((_, dst) <- mapPaths(resourceSources.value, resourceExclusions.value, resourcePageFilter.value))
+      for((src, dst) <-resourceMap.value if resourcePageFilter.value accept src)
         yield dst
     }
   )
